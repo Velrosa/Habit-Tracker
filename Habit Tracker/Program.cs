0164467788b41b2fb587dac23b60340ae437d481
Velrosa <steezie21@hotmail.co.uk>
2022-03-21 20:11:07 +0000

    Created Database class with SQLite, Created Local Database File, Created Menu Selector,

--- Habit Tracker/Program.cs
+++ Habit Tracker/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Data.SQLite;
 
 namespace Habit_Tracker
 {
@@ -6,7 +8,21 @@ namespace Habit_Tracker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Database databaseObject = new Database();
+            MainMenu();
+            string selector = Convert.ToString(Console.ReadLine());
+            Console.WriteLine(selector);
+        }
+
+        public static void MainMenu()
+        {
+            Console.WriteLine("\nMAIN MENU\n\n" +
+                "What would you like to do?\n\n" +
+                "Type 0 to Close Application.\n" +
+                "Type 1 to View All Records.\n" +
+                "Type 2 to Insert Record.\n" +
+                "Type 3 to Delete Record.\n" +
+                "Type 4 to Update Record.\n");
         }
     }
 }
